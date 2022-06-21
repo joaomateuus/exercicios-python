c4be089afe490ee56d0c1a6a5099a8283c53e7e8
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-06-21 16:20:46 -0300

    13.py

---
+++ 13_exercicio.py
@@ -0,0 +1,7 @@
+#Faca um programa q receba um salario e o retorne com o reajuste salarial de 15%
+
+salario_atual = float(input('Qual o salario do funcionario ?  '))
+
+salario_ajustado = salario_atual + (salario_atual*15/100)
+
+print(f'O salario do funcionario era {salario_atual:.3f}, e após ao reajuste de 15% passou a ser {salario_ajustado:.3f}')
