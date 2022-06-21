8d8b8c9d7a3210c6d8a21a539b8732c85c018384
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-06-20 23:45:00 -0300

    exercise 7

--- 07_exercicio.py
+++ 07_exercicio.py
@@ -4,23 +4,31 @@ nota2 = 0
 paralela = 0
 operador = 1
 while operador != 0:
+    print('0-Sair')
     print('1-Calcular media')
     operador = int(input('Digite uma opcao: '))
     if operador == 1:
         nota1 = float(input('Digite a primeira nota: '))
-        nota2 = float(input('Digite a segunda nota'))
+        nota2 = float(input('Digite a segunda nota:  '))
         if(nota1 < 7 or nota2 < 7 ):
-            paralela = float(input('Digite sua nota paraleal'))  
+            paralela = float(input('Digite sua nota paraleal:  '))  
             if(paralela > nota1 or nota2):
                 print('Parabéns voce passou na paralela')
                 if(paralela > nota1):
                     print('A sua primeira nota sera substituida')
                     paralela = nota1
+                    print('Sua média é: ', float(nota1+nota2) /2)
                 else:
                     print('A sua segunda nota sera substituida')
                     paralela = nota2
-                    pass
-        print('Sua média é: ', nota1+nota2/3)     
-    else:
-        print('Sua média é: ', nota1+nota2/3)
+                    print('Sua média é: ', float(nota1+nota2) /2)
+            else:
+                print('Ahh voce n passou na parela')
+                print('Sua média parcial é: ', float(nota1+nota2) /2)     
+        else:
+            print('Sua média parcial é: ', float(nota1+nota2) /2)
+    elif operador == 0:
+        break
+    
+    
                        
