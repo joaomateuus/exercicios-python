4e8beae55c1d47495c6dc755080829320f53adb9
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-06-20 22:35:50 -0300

    exercise 6

---
+++ 06_exercicio.py
@@ -0,0 +1,10 @@
+# Faca um programa que digite um numero inteiro e retorne seu dobro, triplo e raiz quadrada
+
+numero = int(input('Digite um numero inteiro:  '))
+
+print('O dobro desse numero é:', numero*2)
+print('O triplo desse numero é:', numero*3)
+#N elevado a meio, entre parenteses para o 1/2 ser resolvido primeiro
+print('A raiz quadrada desse número é:  ', numero**(1/2))
+
+
