9a1674e519b59166b5c055a8646cf6756c6566b8
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-06-20 22:28:52 -0300

    exercise 5

---
+++ 05_exercicios.py
@@ -0,0 +1,8 @@
+#Faca um programa que leia um numero inteiro e mostre seu antecessor e sucessor
+
+inteiro = int(input('Digite um numero inteiro:  '))
+ante = inteiro -1
+suce = inteiro +1
+
+print(f'O sucessor e antecessor do numero {inteiro}, sao respectivamente {suce} e {ante}')
+
