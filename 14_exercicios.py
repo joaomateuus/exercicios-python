b225b3aca7e6b191382ed59118bf711fa0ea0465
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-06-22 11:02:16 -0300

    exercise 14

---
+++ 14_exercicios.py
@@ -0,0 +1,5 @@
+valor_celsius = float(input('Digite a temperatura em graus celsius ?  '))
+
+valor_farenheit = (valor_celsius*1.8)+32
+
+print(f'A temperatura de {valor_celsius} é que vale a {valor_farenheit} farenheits')
