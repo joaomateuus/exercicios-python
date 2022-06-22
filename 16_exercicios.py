2341bc2adf409dd7e27630cf8d14592d9a0b4ffd
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-06-22 14:49:19 -0300

    exercise 16

---
+++ 16_exercicios.py
@@ -0,0 +1,9 @@
+#Faca um programa que receba um numero quebrado e retorne apenas sua parte inteira
+
+numero = float(input('Digite um numero decimal:  '))
+
+numero_inteiro = int(numero)
+
+print(f'O numero digitado foi {numero} e seu valor inteiro é: {numero_inteiro} ')
+
+
